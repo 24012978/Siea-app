41f5b97cbf37d14fb6a9f56d8e2b190d9d586171
24012978 <23012978@es.uveg.edu.mx>
2026-08-20 22:13:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ h+='<p>Acceso ilimitado a todas las materias.</p>'
 h+='<a href="https://wa.me/528110290152" target="_blank" style="display:block;background:#a855f7;color:white;padding:12px;border-radius:10px;text-decoration:none;font-weight:bold;">PAGAR SUSCRIPCION - $49 MXN</a>'
 h+='</div>'
 h+='<div class=foot>PLATAFORMA DE USO INTERNO Y CONTROL INSTITUCIONAL | S.I.E.A. 2026</div>'
- return h
+return h
 
 def base(t,b):
  u=session.get("user","")
